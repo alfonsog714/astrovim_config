e3498c4b32a161332fba82a9ff98350e7ad1c9cb
Alfonso Garcia <alfonso.garcia2324@gmail.com>
2023-04-04 09:33:21 -0700

    cleaned up user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,14 +1,6 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
